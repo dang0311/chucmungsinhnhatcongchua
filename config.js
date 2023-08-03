70871cdf60ddb899a7426e904795a3a128728ee1
ares311 <nguyenhaidang299@gmail.com>
2023-08-03 21:40:25 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 18-8';
+const titleCard = 'Tặng công chúa của anh❤';
+const contentCard = 'Chúc công chúa của anh sinh nhật vui vẻ luôn luôn hạnh phúc.Mong là sau này anh có thể luôn ở bên em chăm sóc cho em.Yêu em❤';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
